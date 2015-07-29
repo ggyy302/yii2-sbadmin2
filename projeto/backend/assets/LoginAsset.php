e508da2ee6fbc529fb2f43d22a68c19765ce8e01
alissoncti <alisson.campelo@ctiti..com.br>
2015-07-29 14:03:15 -0300

    Página Inicial
    Mudança na página inicial - Dashboard

--- projeto/backend/assets/LoginAsset.php
+++ projeto/backend/assets/LoginAsset.php
@@ -13,27 +13,14 @@ use yii\web\AssetBundle;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class AppAsset extends AssetBundle
+class LoginAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/bootstrap.min.css',
-        'css/metisMenu.min.css',
-        'css/sb-admin-2.css',
-        'css/timeline.css',
-        'css/morris.css',
-        'css/font-awesome.min.css',
-        'css/site.css',
+        //'css/site.css',
     ];
     public $js = [
-        'js/jquery.min.js',
-        'js/bootstrap.min.js',
-        'js/metisMenu.min.js',
-        'js/raphael-min.js',
-        'js/morris.min.js',
-        'js/morris-data.js',
-        'js/sb-admin-2.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
