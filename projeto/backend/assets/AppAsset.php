ad79950c29a378253960e233f6d3f43249373a85
alissoncti <alisson.campelo@ctiti.com.br>
2015-07-29 21:44:02 -0300

    Modificado Layout Básico
    Modificado layout básico.
    Iniciando montagem do Dashboard

--- projeto/backend/assets/AppAsset.php
+++ projeto/backend/assets/AppAsset.php
@@ -19,21 +19,9 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/bootstrap.min.css',
-        'css/metisMenu.min.css',
-        'css/sb-admin-2.css',
-        'css/timeline.css',
-        'css/morris.css',
-        'css/font-awesome.min.css',
         'css/site.css',
     ];
     public $js = [
-        'js/jquery.min.js',
-        'js/bootstrap.min.js',
-        'js/metisMenu.min.js',
-        'js/raphael-min.js',
-        'js/morris.min.js',
-        'js/morris-data.js',
-        'js/sb-admin-2.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
