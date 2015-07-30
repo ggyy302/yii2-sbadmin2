865f4f496015b748fca0345bb731b1ceb8e217bf
alissoncti <alisson.campelo@ctiti..com.br>
2015-07-30 18:11:24 -0300

    Ajuste simples

--- projeto/backend/views/layouts/main.php
+++ projeto/backend/views/layouts/main.php
@@ -21,6 +21,8 @@ DashboardAsset::register($this);
 <body>
     <?php $this->beginBody() ?>
     <div id="wrapper">
+
+    <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -29,11 +31,14 @@ DashboardAsset::register($this);
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="?r=site/index">Dashboard - Painel de Controle</a>
+                <a class="navbar-brand" href="?r=site/index">SB Admin v2.0</a>
             </div>
+            <!-- /.navbar-header -->
             <ul class="nav navbar-top-links navbar-right">
                 <li class="dropdown">
-                    1111
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="glyphicon glyphicon-envelope"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
                 </li>
             </ul>
             <div class="navbar-default sidebar" role="navigation">
